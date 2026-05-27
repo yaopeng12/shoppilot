140aed7241dd9bcaa38d06a4c9bba78e0aef6ae7
Mason.yao <yaopeng1227@outlook.com>
2026-05-28 02:59:01 +0800

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "@/components/tiktok-adgen/providers";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -80,6 +81,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
+        <GoogleAnalytics gaId="G-8PD0RQZ23K" />
       </body>
     </html>
   );
